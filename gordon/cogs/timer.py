b565368bb36869c54919b4e350038acb9955a904
lkim10 <76914532+lkim10@users.noreply.github.com>
2021-01-23 09:23:04 -0500

    Update timer.py

--- gordon/cogs/timer.py
+++ gordon/cogs/timer.py
@@ -1,7 +1,30 @@
 import discord
-from discord.ext import commands
+from discord.ext. import commands,tasks
 
+client=commands.Bot(command_prefix='>')
 
-class Timer(commands.Cog):
-    def __init__(self, bot):
-        self.bot = bot
+@climent.event
+async def timer(ctx, seconds):
+		try:
+		secondint=int(seconds)
+		if secondint>300;
+		await ctx.send("Cannot go over 5 minutes.")
+		raise BaseException
+		if secondint<=0:
+		await ctx.send("Negatives are not available.")
+		raise BaseException
+
+message=await ctx.send({"timer:{seconds}"})
+
+while true:
+secondint-=1
+if secondint==0:
+await message.edit(content="End!")
+break
+
+await message.edit(content=f"Timer: {seconint}")
+await asyncio.sleep(1)
+await ctx.send(f"{ctx.author.mention}, Countdown Over!")
+
+except ValueError:
+await ctx.send("Enter a number.")
