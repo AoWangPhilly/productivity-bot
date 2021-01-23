ec8aa52411915cf957330026b991dc9abbbe455f
AoWangDrexel <aw3338@drexel.edu>
2021-01-22 23:42:56 -0500

    Add in pickle function to save Python lists

--- gordon/src/sarcasm/scraper.py
+++ gordon/src/sarcasm/scraper.py
@@ -7,17 +7,34 @@ from bs4 import BeautifulSoup
 import requests
 from random import choice
 import json
+import pickle
+from typing import List
+from os.path import join
 
 # ---------------------------- URLS --------------------------------
 MEME_URL = 'https://www.scoopwhoop.com/humour/gordon-ramsay-memes/'
 QUOTES_URL = 'https://www.scarymommy.com/gordon-ramsay-memes-quotes/'
 # ------------------------------------------------------------------
 
-
 # --------------------------- GIPHY KEY ----------------------------
 with open('giphy_key', 'r') as f:
     KEY = f.read()
 # ------------------------------------------------------------------
+
+
+def save_as_pickle(lst: List[str], fname: str) -> None:
+    '''Save Python lists as pickle files, therefore it'll be easier
+       when Discord bot goes in to read in random meme or quote.
+
+    lst: the list of URL's of images or quotes
+    lst: list of strings
+    fname: the filename of the pickle file
+    fname: str
+    '''
+    fname = join('..', '..', 'db', 'img', fname+'.pkl')
+    with open(fname, 'wb') as f:
+        pickle.dump(lst, f)
+
 
 if __name__ == '__main__':
     # Make requests to pages
@@ -45,3 +62,6 @@ if __name__ == '__main__':
         f'http://api.giphy.com/v1/gifs/search?q=gordon+ramsay&api_key={KEY}&limit={LIMIT}').content
     gifs = json.loads(encoded_gifs.decode('utf8'))
     gifs_lst = [gif['embed_url']for gif in gifs['data']]
+
+    save_as_pickle(img_url_lst, 'Memes')
+    save_as_pickle(gifs_lst, 'GIFs')
