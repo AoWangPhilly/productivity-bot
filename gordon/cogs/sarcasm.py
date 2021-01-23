75a387231b86a7a15a9ebc12389158d65f807ebf
AoWangDrexel <aw3338@drexel.edu>
2021-01-23 09:02:16 -0500

    Test out sarcasm

--- gordon/cogs/sarcasm.py
+++ gordon/cogs/sarcasm.py
@@ -1,19 +1,29 @@
 import discord
 from discord.ext import commands
+import pickle
+from random import choice
+from os.path import join
 
 
 class Sarcasm(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command()
-    async def meme(self, ctx):
-        pass
+    def _open_pickle(self, fname):
+        pickle_path = join('db', fname+'.pkl')
+        with open(pickle_path, 'rb') as f:
+            pickle_lst = pickle.load(f)
+        return pickle_lst
 
     @commands.command()
+    async def meme(self, ctx):
+        await ctx.send(choice(self._open_pickle('Memes')))
+
+    # TODO: FIX GIF SOMETiMES PRINTS TWO LINKS
+    @commands.command()
     async def gif(self, ctx):
-        pass
+        await ctx.send(choice(self._open_pickle('GIFs')))
 
     @commands.command()
     async def quote(self, ctx):
-        pass
+        await ctx.send(choice(self._open_pickle('Quotes')))
