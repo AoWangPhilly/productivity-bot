d5c6bfdc0d0c8253e96e340b8408e21e6d9a398e
AoWangDrexel <aw3338@drexel.edu>
2021-01-23 09:02:22 -0500

    Add sarcasm cog

---
+++ gordon/main.py
@@ -0,0 +1,35 @@
+import discord
+from discord.ext import commands
+from cogs.sarcasm import Sarcasm
+
+import logging
+import os
+
+# -------------------- DISCORD LOGGING ------------------------
+logger = logging.getLogger('discord')
+logger.setLevel(logging.DEBUG)
+handler = logging.FileHandler(
+    filename='discord.log', encoding='utf-8', mode='w')
+handler.setFormatter(logging.Formatter(
+    '%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+logger.addHandler(handler)
+# -------------------------------------------------------------
+
+# -------------------- SECRET KEY -----------------------------
+with open('secret_key', 'r') as f:
+    SECRET_KEY = f.read()
+# -------------------------------------------------------------
+
+bot = commands.Bot(command_prefix='>')
+
+# ----------------------- ADD COGS ----------------------------
+bot.add_cog(Sarcasm(bot))
+# -------------------------------------------------------------
+
+
+@bot.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(bot))
+
+
+bot.run(SECRET_KEY)
