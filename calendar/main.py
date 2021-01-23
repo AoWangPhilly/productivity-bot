b0532ca38673fe0c2949d4c493f4da6e2c5fd5bf
Joey <jh3759@drexel.edu>
2021-01-23 00:06:55 -0500

    Update main.py
    Basic script with scrolling calendar

--- calendar/main.py
+++ calendar/main.py
@@ -1,3 +1,55 @@
+# Joey Huang
+# 1/22/2021
+# Basic Python script using the calendar module that allows the user to scroll
+# back and forth displaying different months. Loops until user enters STOP.
+
 import calendar
 
-print(calendar.calendar(2021,2,1,6))
+def printCalendar(intYear, intMonth):
+    print(calendar.month(intYear,intMonth))    
+    print("< | >\n")
+    print(">>> Enter STOP to exit...\n")
+
+if __name__ == "__main__":
+    print("=================================================================")
+    print(">>> Welcome to your interactive calendar! Are you ready to start?")
+
+    # Not case sensitive
+    reply = input(">>> Enter YES or NO: \n")
+    if reply.lower() == "yes" or reply.upper() == "YES":
+        year = int(input("Enter year: \n"))
+        month = int(input("Enter month: \n"))
+        printCalendar(year, month)
+        reply = input()
+
+        # Loops until STOP/stop is entered
+        while not(reply.lower() == "stop" or reply.upper() == "STOP"):
+            if reply == "<":
+                # Accounts for January and December respectively
+                if month == 1:  
+                    month = 12
+                    year -= 1
+                else:
+                    month -= 1
+            else:
+                if month == 12:
+                    month = 1
+                    year += 1
+                else:
+                    month += 1
+            printCalendar(year, month)
+            reply = input()
+
+    elif reply.lower() == "no" or reply.upper() == "NO":
+        print(">>> Good bye!")
+    else:
+        print(">>> Invalid response.")
+    print("=================================================================")
+
+
+
+
+
+
+    #print(calendar.calendar(2021,2,1,6))
+    #print(calendar.month(2021,2,1,1))
