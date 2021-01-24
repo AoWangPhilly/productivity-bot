6a6099da0726925bcfe9213c421a2d8b12f29b67
AoWangDrexel <aw3338@drexel.edu>
2021-01-23 22:16:47 -0500

    Fix TodoList

--- gordon/cogs/todolist.py
+++ gordon/cogs/todolist.py
@@ -7,40 +7,41 @@
 import discord
 from discord.ext import commands
 
+
 class todoList(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.__currList = []
-        self.__currIndex = 1
-        self.__tempItem = None
+        self.__tasks = []
 
     @commands.command()
     async def todo(self, ctx, comm, *arg):
         # >todo add <task>
         # Adds a task to to-do list
-        if comm == "add": 
+        if comm == "add":
             task = ' '.join(arg)
-            self.__currList.append(task)
-            await ctx.send('Added "' + task + '" as a task!')      
-        # >todo show 
-        # Shows current to-do list     
+            self.__tasks.append(task)
+            await ctx.send(f'Added {task} as a task!')
+        # >todo show
+        # Shows current to-do list
         elif comm == "show":
-            taskList = "\n".join(self.__currList)
+            todo_list_msg = ''
+            for i, task in enumerate(self.__tasks, start=1):
+                todo_list_msg += f'{i}. {task}\n'
             await ctx.send('>>> To-Do List\n')
-            await ctx.send('```To-Do List \n' + str(taskList) + '```')
-        
+            await ctx.send(f'```To-Do List \n{todo_list_msg}```')
+
         # >todo remove <task number>
         # Removes item from list
         elif comm == "remove":
-            targetInd = ''.join(arg)
-            tempInd = int(targetInd)
-            targetInd = tempInd - 1
-            self.__currList.pop(targetInd)
-            await ctx.send('Removed task ' + str(tempInd) + "!\nGood work idiot sandwich!")
-        
+            target_idx = int(''.join(arg)) - 1
+            if len(self.__tasks):
+                task = self.__tasks.pop(target_idx)
+                await ctx.send(f'Removed task: {task}!\nGood work idiot sandwich!')
+
         # Invalid command if anything else is entered
         else:
             await ctx.send('Invalid command.')
+
 
 def setup(bot):
     bot.add_cog(todoList(bot))
